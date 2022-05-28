0989d77a0d3840c193b566810ce17c1788576f8a
zzzzsb <ji12subin@gmail.com>
2022-05-29 05:45:55 +0900

    voca

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Day from './component/Day';
+import DayList from './component/DayList';
+import Header from './component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import EmptyPage from './component/EmptyPage';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path='/create_word' element={<CreateWord />} />
+          <Route path='/create_day' element={<CreateDay />} />
+          <Route path="/*" element={<EmptyPage />} />
+        </Routes>
+      </div>  
+    </BrowserRouter>
   );
 }
 
